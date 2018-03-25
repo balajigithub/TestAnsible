4bff496e3d0b1d2b3ce6097dbfe03fc14720a5b9
balajigithub <mbala38@hotmail.com>
2018-03-25 14:55:15 +1100

    commit

---
+++ 2 Problem Solving/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i.ToString() + Division.ReturnStr(i));
+            }
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
